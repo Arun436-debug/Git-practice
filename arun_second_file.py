04c6ebdfe76acee02f74345e7c0de77ff2416c4e
Arun436-Debug <arunkandukuri94@gmail.com>
2025-01-08 16:42:31 +0530

    this is your second file

---
+++ arun_second_file.py
@@ -0,0 +1,7 @@
+while True:
+    def check_even_or_odd(num) -> str:
+        return "even" if num%2 == 0 else "odd"
+
+    n = int(input('ENer the number to check even or not : '))
+    res = check_even_or_odd(n)
+    print(res)
